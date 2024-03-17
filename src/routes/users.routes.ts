7151a1c215db78cb958c44673cecdfb57c2cf03b
EverSilva <everson.silva2706@gmail.com>
2024-03-16 22:50:55 -0300

    feat: initial commit

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,31 @@
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+import { randomUUID } from 'crypto'
+import { z } from 'zod'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const users = await knex('users').select()
+
+    return { data: users }
+  })
+
+  app.post('/', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+    })
+
+    const { name, email } = createUserBodySchema.parse(request.body)
+
+    const user = {
+      id: randomUUID(),
+      name,
+      email,
+    }
+
+    await knex('users').insert(user)
+
+    return reply.status(201).send({ user })
+  })
+}
